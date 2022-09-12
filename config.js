e65a0f6d5e48d3ae0bc542a94c8a4655f21d69cf
Atomic1325 <93055019+Atomic1325@users.noreply.github.com>
2022-09-11 21:03:20 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCicRtfjxuCMMpoyhe6NoCNzyrk0z3Jrs",
+  authDomain: "foro-de-quejas-deaf7.firebaseapp.com",
+  projectId: "foro-de-quejas-deaf7",
+  storageBucket: "foro-de-quejas-deaf7.appspot.com",
+  messagingSenderId: "37092545884",
+  appId: "1:37092545884:web:d7f9e89cbad51c78dc5d99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
